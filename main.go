53180d8387cc7bf0aa875d1e71949732cad3e706
Jake Tompkins <jaketompkins91@outlook.com>
2023-09-29 16:44:59 -0700

    realized that command wouldn't actually do anything

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"tg-group-scheduler/telegram"
 
 	"github.com/joho/godotenv"
@@ -11,11 +10,5 @@ func main() {
 	godotenv.Load()
 
 	bot := telegram.NewTelegramBot()
-	err := bot.Listen()
-
-	if err != nil {
-		fmt.Println("Bot listening successfully")
-	} else {
-		fmt.Println("Bot failed to listen")
-	}
+	bot.Listen()
 }
